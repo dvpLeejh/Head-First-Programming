861a79c813f88286fe046e88a1291cadf768c743
Jihwan LEE <42956142+dvpLeejh@users.noreply.github.com>
2020-09-01 21:16:02 +0900

    Create urllib-finding-time.py

---
+++ ch02/urllib-finding-time.py
@@ -0,0 +1,19 @@
+import urllib.request
+
+import time
+
+price = 99.9
+
+while price > 4.74:
+    time.sleep(1)
+    page = urllib.request.urlopen("http://beans.itcarlow.ie/prices-loyalty.html")
+    text = page.read().decode("utf-8")
+
+    where = text.find(">$")
+
+    start_of_price = where+2
+    end_of_price = start_of_price+4
+
+    price = float(text[start_of_price:end_of_price])
+
+print("buy!")
