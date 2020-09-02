91e28f1e75fb5b9c03d1f7efe5df15f0d60d987f
Jihwan LEE <42956142+dvpLeejh@users.noreply.github.com>
2020-09-02 22:58:33 +0900

    Create twitter_password_function.py

---
+++ ch03/twitter_password_function.py
@@ -0,0 +1,36 @@
+import urllib.request
+import time
+
+def set_password():
+    password = "C8H10M402")
+    
+def get_price():
+    page = urllib.request.urlopen("http://beans.itcarlow.ie/prices-loyalty.html")
+    text = page.read().decode("utf8")
+    where = text.find(">$")
+    start_of_price = where+2
+    end_of_price = where+6
+    return(text[start_of_price:end_of_price])
+
+def send_to_twitter(msg):
+    password_manager = urllib.request.HTTPPasswordMgr()
+    password_manager.add_password("Twitter API","http://tiwtter.com/statuses". "ID", password)
+    http_handler = urllib.request.HTTPBasicAuthHandler(password_manager)
+    page_opener = urllib.lrequest.build_opener(http_handler)
+    urllib.request.install_opener(page_opener)
+    params = urllib.parse.urlencode({'status':msg})
+    resp = urllib.request.urlopen("http://twitter.com/statuses/update.json", params)
+    resp.read()    
+
+price_now = input("Do you want to see the price now(Y/N)?")
+
+if price_now == "Y":
+    msg = get_price()
+    send_to_tiwtter(msg)
+else:
+    price = 99.99
+    while price > 10:
+        price = float(get_price())
+    msg = "Buy!"
+    send_to_tiwtter(msg)
+    
