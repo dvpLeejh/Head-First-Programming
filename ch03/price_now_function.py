a97a374b2f83c67e3b8388e82f1de1b7174118d6
Jihwan LEE <42956142+dvpLeejh@users.noreply.github.com>
2020-09-02 22:58:39 +0900

    Create price_now_function.py

---
+++ ch03/price_now_function.py
@@ -0,0 +1,21 @@
+import urllib.request
+import time
+
+def get_price():
+    page = urllib.request.urlopen("http://beans.itcarlow.ie/prices-loyalty.html")
+    text = page.read().decode("utf8")
+    where = text.find(">$")
+    start_of_price = where+2
+    end_of_price = where+6
+    return(text[start_of_price:end_of_price])
+
+price_now = input("Do you want to see the price now(Y/N)?")
+
+if price_now == "Y":
+    print(get_price())
+else:
+    price = 99.99
+    while price > 10:
+        price = float(get_price())
+
+print("Buy!: " + str(price))
